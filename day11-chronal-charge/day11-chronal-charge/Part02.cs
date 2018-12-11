e57da965e92ff9c0191bd3809221574ec0ac20b6
Hampus Berggren <hampus.berggren@datscha.com>
2018-12-11 16:49:17 +0100

    Day11 Incomplete

--- day11-chronal-charge/day11-chronal-charge/Part02.cs
+++ day11-chronal-charge/day11-chronal-charge/Part02.cs
@@ -13,7 +13,7 @@ namespace day11_chronal_charge {
             public Point Position { get; set; }
         }
 
-        class FuelCellAggregate {
+        struct FuelCellAggregate {
             public long PowerLevel { get; set; }
         }
 
@@ -21,98 +21,56 @@ namespace day11_chronal_charge {
         delegate void PerFuelCellAggregateDelegate(FuelCellAggregate pCell, int pX, int pY);
 
         static Size FuelCellGridSize;
-        static Size CellAreaToCheck;
-        static ManualResetEvent[] resetEvents;
 
-        static volatile List<ThreadResult> results;
+        static Dictionary<int, FuelCellAggregate[,]> history;
 
         public static void Run() {
-            ThreadPool.SetMaxThreads(6, 6);
-            resetEvents = new ManualResetEvent[60];
-            for (int i = 0; i < 60; i++) { resetEvents[i] = new ManualResetEvent(false); }
-            results = new List<ThreadResult>();
-
-            for (int size = 1; size <= 60; size++) {
-                ThreadPool.QueueUserWorkItem(Task, new Parameters { Input = 8141, Size = size, ResetEvent = size - 1 });
-            }
-
-            WaitHandle.WaitAll(resetEvents);
-            Console.WriteLine("Completed 60.");
-            resetEvents = new ManualResetEvent[60];
-            for (int i = 0; i < 60; i++) { resetEvents[i] = new ManualResetEvent(false); }
-
-            for (int size = 61; size <= 120; size++) {
-                ThreadPool.QueueUserWorkItem(Task, new Parameters { Input = 8141, Size = size, ResetEvent = size - 61 });
-            }
-
-            WaitHandle.WaitAll(resetEvents);
-            Console.WriteLine("Completed 120.");
-            resetEvents = new ManualResetEvent[60];
-            for (int i = 0; i < 60; i++) { resetEvents[i] = new ManualResetEvent(false); }
-
-            for (int size = 121; size <= 180; size++) {
-                ThreadPool.QueueUserWorkItem(Task, new Parameters { Input = 8141, Size = size, ResetEvent = size - 121 });
-            }
-
-            WaitHandle.WaitAll(resetEvents);
-            Console.WriteLine("Completed 180.");
-            resetEvents = new ManualResetEvent[60];
-            for (int i = 0; i < 60; i++) { resetEvents[i] = new ManualResetEvent(false); }
-
-            for (int size = 181; size <= 240; size++) {
-                ThreadPool.QueueUserWorkItem(Task, new Parameters { Input = 8141, Size = size, ResetEvent = size - 181 });
-            }
-
-            WaitHandle.WaitAll(resetEvents);
-            Console.WriteLine("Completed 240.");
-            resetEvents = new ManualResetEvent[60];
-            for (int i = 0; i < 60; i++) { resetEvents[i] = new ManualResetEvent(false); }
-
-            for (int size = 241; size <= 300; size++) {
-                ThreadPool.QueueUserWorkItem(Task, new Parameters { Input = 8141, Size = size, ResetEvent = size - 241 });
-            }
-
-            WaitHandle.WaitAll(resetEvents);
-            Console.WriteLine("Completed 300.");
-
-            var largest = results.OrderBy(r => r.PowerLevel).First();
-
-            Console.WriteLine($"{largest.Position.X},{largest.Position.Y},{largest.Size}");
-        }
-
-        struct Parameters {
-            public int Input { get; set; }
-            public int Size { get; set; }
-            public int ResetEvent { get; set; }
-        }
-
-        struct ThreadResult {
-            public Point Position { get; set; }
-            public int Size { get; set; }
-            public long PowerLevel { get; set; }
-        }
-
-        public static void Task(object pState) {
-            var parameters = (Parameters)pState;
-            var result = RunOnce(parameters.Input, new Size(parameters.Size, parameters.Size));
-
             long largestResult = long.MinValue;
             int largestSize = 0;
             var point = Point.Empty;
+            history = new Dictionary<int, FuelCellAggregate[,]>();
 
-            if (result.PowerLevel > largestResult) {
-                largestSize = parameters.Size;
-                largestResult = result.PowerLevel;
-                point = result.Coordinate;
+            int input = 8141;
+
+            FuelCellGridSize = new Size(300, 300);
+
+            if (fuelCells == null) {
+                fuelCells = new FuelCell[FuelCellGridSize.Width, FuelCellGridSize.Height];
+                var fuelCellAggregates = new FuelCellAggregate[FuelCellGridSize.Width, FuelCellGridSize.Height];
+
+                var perFuelCell = new Action<PerFuelCellDelegate>(dlgt => {
+                    for (int x = 1; x <= FuelCellGridSize.Width; x++) {
+                        for (int y = 1; y <= FuelCellGridSize.Height; y++) {
+                            if (fuelCells[x - 1, y - 1] == null) {
+                                fuelCells[x - 1, y - 1] = new FuelCell();
+                            }
+                            dlgt?.Invoke(fuelCells[x - 1, y - 1], x, y);
+                        }
+                    }
+                });
+
+                history.Add(1, new FuelCellAggregate[300, 300]);
+
+                perFuelCell((cell, x, y) => {
+                    cell.Position = new Point(x, y);
+                    cell.RackId = x + 10;
+                    var powerLevel = CalculatePowerLevel(cell.RackId, y, input);
+                    cell.PowerLevel = powerLevel;
+                    history[1][x-1, y-1] = new FuelCellAggregate { PowerLevel = powerLevel };
+                });
             }
 
-            results.Add(new ThreadResult {
-                Position = point,
-                Size = largestSize,
-                PowerLevel = largestResult
-            });
+            for (int size = 2; size <= 300; size++) {
+                var result = RunOnce(size);
 
-            resetEvents[parameters.ResetEvent].Set();
+                if (result.PowerLevel > largestResult) {
+                    largestSize = size;
+                    largestResult = result.PowerLevel;
+                    point = result.Coordinate;
+                }
+            }
+
+            Console.WriteLine($"{point.X},{point.Y},{largestSize}");
         }
 
         internal struct Result {
@@ -120,67 +78,50 @@ namespace day11_chronal_charge {
             public long PowerLevel { get; set; }
         }
 
-        public static Result RunOnce(int pInput, Size pSize) {
-            CellAreaToCheck = pSize;
-            FuelCellGridSize = new Size(300, 300);
+        static FuelCell[,] fuelCells;
 
-            var fuelCells = new FuelCell[FuelCellGridSize.Width, FuelCellGridSize.Height];
-            var fuelCellAggregates = new FuelCellAggregate[FuelCellGridSize.Width, FuelCellGridSize.Height];
+        public static Result RunOnce(int pSize) {
+            int previousSize = pSize - 1;
 
-            var perFuelCell = new Action<PerFuelCellDelegate>(dlgt => {
-                for (int x = 1; x <= FuelCellGridSize.Width; x++) {
-                    for (int y = 1; y <= FuelCellGridSize.Height; y++) {
-                        dlgt?.Invoke(fuelCells[x - 1, y - 1], x, y);
+            var previousFuelCellAggregates = new FuelCellAggregate[0, 0];
+
+            if (history.ContainsKey(previousSize)) {
+                previousFuelCellAggregates = history[previousSize];
+            }
+
+            history.Add(pSize, new FuelCellAggregate[300 - pSize + 1, 300 - pSize + 1]);
+
+            long largestPower = long.MinValue;
+            var largestPoint = Point.Empty;
+
+            for (int x = 0; x < FuelCellGridSize.Width - pSize + 1; x++) {
+                for (int y = 0; y < FuelCellGridSize.Height - pSize + 1; y++) {
+                    long powerlevel = 0;
+                    for (int sy = 0; sy < pSize - 1; sy++) {
+                        var rightCell = fuelCells[x + pSize - 1, y + sy].PowerLevel;
+                        powerlevel += rightCell;
                     }
-                }
-            });
 
-            var perFuelCellAggregate = new Action<PerFuelCellAggregateDelegate>(dlgt => {
-                for (int x = 0; x < FuelCellGridSize.Width; x++) {
-                    for (int y = 0; y < FuelCellGridSize.Height; y++) {
-                        dlgt?.Invoke(fuelCellAggregates[x, y], x, y);
+                    for (int sx = 0; sx < pSize - 1; sx++) {
+                        var bottomCell = fuelCells[x + sx, y + pSize - 1].PowerLevel;
+                        powerlevel += bottomCell;
                     }
-                }
-            });
 
-            perFuelCell((cell, x, y) => {
-                fuelCells[x - 1, y - 1] = new FuelCell();
-            });
+                    // corner
+                    powerlevel += fuelCells[x + pSize - 1, y + pSize - 1].PowerLevel;
 
-            perFuelCell((cell, x, y) => {
-                cell.Position = new Point(x, y);
-                cell.RackId = x + 10;
-                var powerLevel = CalculatePowerLevel(cell.RackId, y, pInput);
-                cell.PowerLevel = powerLevel;
-            });
-
-            perFuelCellAggregate((cell, x, y) => {
-                fuelCellAggregates[x, y] = new FuelCellAggregate();
-            });
-
-            for (int x = 0; x < FuelCellGridSize.Width - CellAreaToCheck.Width - 1; x++) {
-                for (int y = 0; y < FuelCellGridSize.Height - CellAreaToCheck.Height - 1; y++) {
-                    for (int sx = 0; sx < CellAreaToCheck.Width; sx++) {
-                        for (int sy = 0; sy < CellAreaToCheck.Height; sy++) {
-                            fuelCellAggregates[x, y].PowerLevel = fuelCellAggregates[x, y].PowerLevel + fuelCells[x + sx, y + sy].PowerLevel;
-                        }
+                    if (powerlevel > largestPower) {
+                        largestPower = powerlevel;
+                        largestPoint = new Point(x, y);
                     }
+
+                    history[pSize][x, y] = new FuelCellAggregate { PowerLevel = history[previousSize][x, y].PowerLevel + powerlevel };
                 }
             }
 
-            long largestPowerCellCenter = long.MinValue;
-            var largestPowerCellTopLeft = Point.Empty;
-
-            perFuelCellAggregate((cell, x, y) => {
-                if (cell.PowerLevel > largestPowerCellCenter) {
-                    largestPowerCellCenter = cell.PowerLevel;
-                    largestPowerCellTopLeft = new Point(x, y);
-                }
-            });
-
             return new Result {
-                Coordinate = new Point { X = largestPowerCellTopLeft.X + 1, Y = largestPowerCellTopLeft.Y + 1 },
-                PowerLevel = largestPowerCellCenter
+                Coordinate = new Point { X = largestPoint.X + 1, Y = largestPoint.Y + 1 },
+                PowerLevel = largestPower
             };
         }
 
--- day11-chronal-charge/day11-chronal-charge/Program.cs
+++ day11-chronal-charge/day11-chronal-charge/Program.cs
@@ -3,8 +3,8 @@
 namespace day11_chronal_charge {
     class Program {
         static void Main(string[] args) {
-            Part01.Run();
-            Console.WriteLine("----------------");
+            //Part01.Run();
+            //Console.WriteLine("----------------");
             Part02.Run();
             Console.WriteLine("----------------");
             Console.WriteLine("Press any key to exit..");
